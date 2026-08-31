40f0e5d9047f19d594a157277aab27b74748aa16
endrue <joltzstudios@gmail.com>
2026-08-31 09:28:47 +0800

    Update base path in vite.config.js
    Change base path for deployment on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // Using './' ensures all assets load relatively on GitHub Pages
+  base: '/water7/',
   plugins: [react()],
-  server: { host: '127.0.0.1', port: 5173 },
 })
